43dadf1623cb87ac4fb24548c12d3b6f7f9378c2
“Arpita <arpitakumari10653@gmail.com>
2025-06-18 15:27:28 +0530

    object attribute and class attribute

---
+++ 01_class.py
@@ -0,0 +1,14 @@
+class Employee:
+    language = "Py"      # this is a class attribute
+    salary = 1200000
+
+arpita = Employee() 
+arpita.name = "Arpita"  
+print( arpita.name, arpita.language, arpita.salary)
+
+ridhi = Employee()
+ridhi.name = "Ridhima"
+print(ridhi.name, ridhi.salary, ridhi.language)
+
+# Here name is object attribute and 
+# salary and language are class attributes as they directly belong to the class
